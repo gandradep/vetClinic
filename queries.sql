af8120724117fdc3c4a1c3428d45f445232b55e3
gandradep <eventsonido@gmail.com>
2022-12-06 05:13:42 +0100

    Fix query

--- queries.sql
+++ queries.sql
@@ -7,7 +7,7 @@ SELECT name from animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-12-3
 /* List the name of all animals that are neutered and have less than 3 escape attempts. */
 SELECT name from animals WHERE neutered = 't' AND escape_attempts < 3;
 /* List the date of birth of all animals named either "Agumon" or "Pikachu". */
-SELECT date_of_birth FROM animals WHERE NOT name='Agumon' AND NOT name='Pikachu';
+SELECT date_of_birth FROM animals WHERE name='Agumon' OR name='Pikachu';
 /* List name and escape attempts of animals that weigh more than 10.5kg */
 SELECT name, escape_attempts FROM animals WHERE  weight_kg > 10.5;
 /* Find all animals that are neutered. */
