1f30c182bc5c7d2c71c36df9ca95c89925426fba
gandradep <eventsonido@gmail.com>
2022-12-08 04:47:12 +0100

    Create owner and species table. Reference animals table to new tables with foreign key

--- schema.sql
+++ schema.sql
@@ -12,3 +12,32 @@ CREATE TABLE animals (
  /* Add a column species of type string to your animals table */ 
 ALTER TABLE animals 
 ADD COLUMN species VARCHAR(250);
+
+/* Create owners table */
+CREATE TABLE owners (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    full_name VARCHAR(250) NOT NULL,    
+    age INT, 
+    PRIMARY KEY (id)
+);
+/* Create table name species */
+CREATE TABLE species (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(250) NOT NULL, 
+    PRIMARY KEY (id)
+);
+
+/* Remove species column from animals table */
+ALTER TABLE animals 
+DROP COLUMN species;
+/* Add column species_id which is a foreign key referencing species table */
+ALTER TABLE animals ADD COLUMN species_id INT;
+ALTER TABLE animals
+ADD CONSTRAINT fk_species
+FOREIGN KEY (species_id) REFERENCES species(id);
+
+/* Add column owner_id which is a foreign key referencing the owners table */
+ALTER TABLE animals ADD COLUMN owner_id INT;
+ALTER TABLE animals
+ADD CONSTRAINT fk_owner
+FOREIGN KEY (owner_id) REFERENCES owners(id);
