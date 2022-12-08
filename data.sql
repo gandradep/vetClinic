81cbbf7d57fc0c8fd68b306fd26383af8eac1776
gandradep <eventsonido@gmail.com>
2022-12-08 10:14:18 +0100

    Add missing owner

--- data.sql
+++ data.sql
@@ -19,6 +19,7 @@ INSERT INTO owners (full_name, age) VALUES ('Jennifer Orwell', 19);
 INSERT INTO owners (full_name, age) VALUES ('Bob', 45);
 INSERT INTO owners (full_name, age) VALUES ('Dean Winchester', 14);
 INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
+INSERT INTO owners (full_name, age) VALUES ('Melody Pond', 77);
 
 /* Data for species table */
 INSERT INTO species (name) VALUES ('Pokemon');
